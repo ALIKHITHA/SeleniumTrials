92fcf05753ab754c45d3e656fed551bcc36c466a
ALIKHITHA <56262291+ALIKHITHA@users.noreply.github.com>
2020-07-09 19:43:35 +0530

    Update fblogin.py

--- fblogin.py
+++ fblogin.py
@@ -3,9 +3,9 @@ from selenium.webdriver.common.keys import Keys
 driver=webdriver.Chrome()
 driver.get('https://en-gb.facebook.com/login')
 eml=driver.find_element_by_name('email')
-eml.send_keys('jathinadhikamsetty@gmail.com')
+eml.send_keys('abc123@gmail.com')
 pwd=driver.find_element_by_name('pass')
-pwd.send_keys('jinnu2646')
+pwd.send_keys('******')
 login=driver.find_element_by_name('login')
 login.click()
 
